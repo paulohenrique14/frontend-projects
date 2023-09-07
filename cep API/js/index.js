658c7520a2f6277c18b1a405717ba6fbad3f2a26
Paulo Henrique <paulobw012345@gmail.com>
2023-09-06 23:41:36 -0300

    js projects

--- cep API/js/index.js
+++ cep API/js/index.js
@@ -6,6 +6,8 @@ const estadoInput    = document.querySelector("#uf");
 const btnConsultaCep = document.querySelector("#consultaCep");
 const addressForm    = document.querySelector(".addressForm")
 const lblCep         = document.querySelector("#lblCep")
+const btnTeste       = document.querySelector(".teste")
+const aside          = document.querySelector(".aside-glass-container")
 
 const url = "https://viacep.com.br/ws/";
 
@@ -23,46 +25,27 @@ btnConsultaCep.addEventListener("click", (e) =>{
     let valorCep = cepInput.value;
     if (valorCep.replace(/\s/g, '').length === 8) {
         showEndereco();
+    }else if (valorCep.replace(/\s/g, '').length === 0){
+        limpaCampo();      
     }else{
         verificacao();
          
-    }
-
-    console.log(valorCep.replace(/\s/g, '').length)
-    
-    
+    }  
 })
 
 async function showEndereco(){
      const endereco = await getEndereco();
 
      if (endereco.logradouro !== undefined){
-        ruaInput.value    = endereco.logradouro
-        bairroInput.value = endereco.bairro
-        cidadeInput.value = endereco.localidade
-        estadoInput.value = endereco.uf
+        ruaInput.value    = endereco.logradouro;
+        bairroInput.value = endereco.bairro;
+        cidadeInput.value = endereco.localidade;
+        estadoInput.value = endereco.uf;
+        desabilitaCampo();
      }else{
         verificacao();
      }
-
-     console.log(endereco.logradouro)
-
-
-
 }
-
-// let pesquisaCep = () =>{
-//     let valorCep = cepInput.value;
-
-//     if (valorCep.replace(/\s/g, '').length === 8){
-//         showEndereco();
-//     }
-// }
-
-// cepInput.addEventListener('keyup', () =>{
-//     console.log('acarcado')
-//     pesquisaCep();
-// })
 
 let verificacao = () => {
     lblCep.textContent = 'Favor digitar o cep corretamente'
@@ -70,9 +53,46 @@ let verificacao = () => {
             lblCep.textContent = 'Digite o seu CEP'   
         }, 2500);
 
-    cepInput.value = '' 
+    // cepInput.value    = ''; 
+    // ruaInput.value    = '';
+    // bairroInput.value = '';
+    // cidadeInput.value = '';
+    // estadoInput.value = '';
+    limpaCampo();
+}
+
+let desabilitaCampo = () =>{
+    if (estadoInput.value.length > 0){
+        estadoInput.disabled = true;
+    }   
+
+    if (cidadeInput.value.length > 0){
+        cidadeInput.disabled = true;
+    }
+
+    if (ruaInput.value.length > 0){
+        ruaInput.disabled = true;
+    }
+
+    if (bairroInput.value.length > 0){
+        bairroInput.disabled = true;
+    }
+}
+
+let limpaCampo = () =>{
+    cepInput.value    = ''; 
     ruaInput.value    = '';
     bairroInput.value = '';
     cidadeInput.value = '';
     estadoInput.value = '';
 }
+
+btnTeste.addEventListener("click", () =>{
+    aside.classList.toggle('move')
+})
+
+aside.addEventListener('click', () =>{
+    aside.classList.remove('move')
+})
+
+
